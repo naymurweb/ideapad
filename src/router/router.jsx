af33fbe554e5031eeb95868dcac5595ae11f1ba3
naymurweb <naymurweb@gmail.com>
2023-03-13 21:42:37 +0600

    router,tailwind css packages setup

---
+++ src/router/router.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/home", element: <h1>this is home page</h1> },
+]);
+
+export default router;
